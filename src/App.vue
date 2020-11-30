4c252149f409373df7e2e8301cf38c87cc6b160d
AngelesCahill <70714713+AngelesCahill@users.noreply.github.com>
2020-11-30 14:54:06 -0300

    subiendo a repositorio

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <Main/>
+  </div>
+</template>
+
+<script>
+import Main from './components/Main.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      datosApi: [],
+      
+    }
+  },
+  components: {
+    Main
+  },
+  created() {
+      fetch ('https://randomuser.me/api')
+        .then (resp => resp.json())
+        .then (result => {
+          this.datosApi = result.results;
+          console.log(this.datosApi)
+          })
+    .catch (error => console.error(error))
+    },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,30 @@
+<template>
+ <div class="main">
+  <div class="container">
+    <div class="row">
+      <div class="card">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  prop: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
